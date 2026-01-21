c5413d9c025fbf6efe370c9acb7c7fa334854671
Matheus Dayan <mdayan.santos@gmail.com>
2026-01-21 19:40:00 -0300

    changes_2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+// Garante que a URL comece com http ou https.
+let API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+if (!API_URL.startsWith("http")) {
+  API_URL = `https://${API_URL}`;
+}
 
 function App() {
   const [users, setUsers] = useState([]);
